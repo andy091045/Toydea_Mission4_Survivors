c78e1de7c5e0104a8082d92adc1891d74e7409b5
andy091045 <andy091045@gmail.com>
2023-05-18 17:25:15 +0900

    add HD.cs Architecture

--- MySurvivorsGame/Assets/Scripts/AbstractScripts/HD.cs
+++ MySurvivorsGame/Assets/Scripts/AbstractScripts/HD.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace HD.Math
@@ -200,4 +199,68 @@ namespace HD.FrameworkDesign
         public Action<T> OnValueChanged;
     }
 
+    public class IOCContainer: MonoBehaviour
+    {
+        private Dictionary<Type, object> mInstances = new Dictionary<Type, object> ();
+
+        public void Register<T>(T instance)
+        {
+            var key = typeof(T);
+
+            if(mInstances.ContainsKey(key))
+            {
+                mInstances[key] = instance;
+            }
+            else
+            {
+                mInstances.Add(key, instance);
+            }
+        }
+
+        public T Get<T>() where T : class
+        {
+            var key = typeof (T);   
+
+            if(mInstances.TryGetValue(key, out var retInstance))
+            {
+                return retInstance as T;
+            }
+
+            return null;
+        }
+    }
+
+    public abstract class Architecture<T> where T : Architecture<T>, new()
+    {
+        private static T mArchitecture;
+
+        static void MakeSureArchitecture()
+        {
+            if(mArchitecture == null)
+            {
+                mArchitecture = new T();
+                mArchitecture.Init();
+            }
+        }
+
+        protected abstract void Init(); 
+
+        private IOCContainer mContainer = new IOCContainer();
+
+        public static T Get<T>() where T : class
+        {
+            MakeSureArchitecture();
+
+            return mArchitecture.mContainer.Get<T>();
+        }
+
+        public void Register<T>(T instance)
+        {
+            MakeSureArchitecture();
+
+            mArchitecture.mContainer.Register<T>(instance); 
+        }
+    }
+
 }
+
