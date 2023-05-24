c9addb0c6b507b045b2bd46bf018910ab1655bba
andy091045 <andy091045@gmail.com>
2023-05-24 19:47:45 +0900

    add MessageSystem to UIManager

--- MySurvivorsGame/Assets/Scripts/Manager/UIManager.cs
+++ MySurvivorsGame/Assets/Scripts/Manager/UIManager.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     DataManager data_;
+    public GameObject Canvas;
 
     void Start()
     {
-        data_ = GameContainer.Get<DataManager>();        
+        data_ = GameContainer.Get<DataManager>();
+        InstantiateUIComponent();
+    }
+
+    void InstantiateUIComponent()
+    {
+        GameObject messageSysem = new GameObject("MessageSystem");
+        messageSysem.transform.parent = Canvas.transform;
+        messageSysem.AddComponent<MessageSystem>();
     }
 
     public void ChooseDevil(string devilName)
