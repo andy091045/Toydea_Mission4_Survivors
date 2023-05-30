10836067e1bae5ab07f4b41c0753f336d83fdfbf
andy091045 <andy091045@gmail.com>
2023-05-30 14:48:41 +0900

    add Start Scene Art Resource

---
+++ MySurvivorsGame/Assets/Scripts/UI/Start/FrameMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameMove : MonoBehaviour
+{
+    Transform target_;
+    [SerializeField] private float speed = 5f;
+
+    public void MoveToTartgetPos(Transform target)
+    {
+        target_ = target;
+        StartCoroutine(MoveToTargetCoroutine());
+    }
+
+    private IEnumerator MoveToTargetCoroutine()
+    {
+        while (Vector3.Distance(transform.position, target_.position) > 0.01f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target_.position, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        // 移动完成后的逻辑
+        Debug.Log("移动完成");
+    }
+}
