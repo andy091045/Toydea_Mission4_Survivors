6b008f34baabea0b716906b6189d38e2cd8ca1af
andy091045 <andy091045@gmail.com>
2023-06-05 16:17:12 +0900

    update unity

--- MySurvivorsGame/Assets/Scripts/UI/ChooseItemManager.cs
+++ MySurvivorsGame/Assets/Scripts/UI/ChooseItemManager.cs
@@ -220,5 +220,12 @@ public class ChooseItemManager : MonoBehaviour
         public string Type;
         public string ObjectName;
     }
+
+    struct ButtonObject
+    {
+        public Button button;
+        public Text text;
+        public Image image;
+    }
 }
 
